cf85f4330a6e3c9684ecbfa020f8c41ac30da957
Matthew-Hu-cmd <760061861@qq.com>
2022-10-08 00:43:40 +0800

    写了一点点线性回归

--- Linear_neural_network/linear_regression.py
+++ Linear_neural_network/linear_regression.py
@@ -4,7 +4,19 @@ import numpy as np
 import torch
 from torch.utils import data
 from d2l import torch as d2l
+from torch import nn
 
 true_w = torch.tensor([2, -3.4])
 true_b = 4.2
-features, labels = d2l.synthetic_data(true_w, true_b, 1000)sum().tolist()
+features, labels = d2l.synthetic_data(true_w, true_b, 1000)
+
+def load_array(data_arrays, batch_size, is_train=True):  #@save
+    """构造一个PyTorch数据迭代器"""
+    dataset = data.TensorDataset(*data_arrays)
+    return data.DataLoader(dataset, batch_size, shuffle=is_train)
+
+batch_size = 10
+data_iter = load_array((features, labels), batch_size)
+
+next(iter(data_iter))
+net = nn.Sequential(nn.Linear(2, 1))
